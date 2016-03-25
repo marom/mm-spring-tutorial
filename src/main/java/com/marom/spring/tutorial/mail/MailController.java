66c26f37bfe81ec0307e25680af2e6ed22a5c816
MaroM <maro.muszynski@gmail.com>
2016-03-25 22:24:30 +0100

    add setter injection example

--- src/main/java/com/marom/spring/tutorial/mail/MailController.java
+++ src/main/java/com/marom/spring/tutorial/mail/MailController.java
@@ -8,13 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MailController {
 
-	@Resource
 	private MailSender mailSender;
 
 	@RequestMapping("/mail")
 	public String sendMail() {
 		mailSender.send("abc@example.com", "Some subject", "the content");
 		return "Mail sent";
+	}
+
+	@Resource
+	public void setMailSender(MailSender mailSender) {
+		this.mailSender = mailSender;
 	}
 
 }
