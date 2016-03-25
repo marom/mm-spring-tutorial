a4f5a51c11a2c9c1fce9aaed79a2091d3e543a47
MaroM <maro.muszynski@gmail.com>
2016-03-25 22:16:18 +0100

    multiple beans- give a name to the bean to match the resource name

--- src/main/java/com/marom/spring/tutorial/mail/MailController.java
+++ src/main/java/com/marom/spring/tutorial/mail/MailController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MailController {
 
-	@Resource(name = "smtpMailSender")
+	@Resource
 	private MailSender mailSender;
 
 	@RequestMapping("/mail")
--- src/main/java/com/marom/spring/tutorial/mail/SmtpMailSender.java
+++ src/main/java/com/marom/spring/tutorial/mail/SmtpMailSender.java
@@ -4,7 +4,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("mailSender")
 public class SmtpMailSender implements MailSender {
 
 	private static final Log log = LogFactory.getLog(SmtpMailSender.class);
