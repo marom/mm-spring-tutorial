8d12b9304ccdaa6661d1fecd804d8dfda4b1ec47
MaroM <maro.muszynski@gmail.com>
2016-03-25 22:27:02 +0100

    constructor injection using @Autowired

--- src/main/java/com/marom/spring/tutorial/mail/MailController.java
+++ src/main/java/com/marom/spring/tutorial/mail/MailController.java
@@ -1,7 +1,6 @@
 package com.marom.spring.tutorial.mail;
 
-import javax.annotation.Resource;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +9,16 @@ public class MailController {
 
 	private MailSender mailSender;
 
+	@Autowired
+	public MailController(MailSender mailSender) {
+		super();
+		this.mailSender = mailSender;
+	}
+
 	@RequestMapping("/mail")
 	public String sendMail() {
 		mailSender.send("abc@example.com", "Some subject", "the content");
 		return "Mail sent";
-	}
-
-	@Resource
-	public void setMailSender(MailSender mailSender) {
-		this.mailSender = mailSender;
 	}
 
 }
